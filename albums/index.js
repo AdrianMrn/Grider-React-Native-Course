53a6351cb8b1d214d7d70d06a45093fe4530ef08
Adriaan Marain <adriaanmarain300@gmail.com>
2018-03-05 17:19:13 +0100

    courses 1 through 54

--- albums/index.js
+++ albums/index.js
@@ -1,13 +1,17 @@
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { AppRegistry, View } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
-const App = () => {
-    return (
-        <Text>Some Text</Text>
-    );
-};
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
 
 // Render it to the device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
